b2b89675d675ae8970add56360e11542e4b478fd
liuxili01 <liuxili@bestseller.com.cn>
2024-07-26 13:45:26 +0800

    rookie-security-yserver 多SecurityFilterChain

--- rookie-security-yserver/src/main/java/com/rookie/bigdata/security/SecurityConfiguration.java
+++ rookie-security-yserver/src/main/java/com/rookie/bigdata/security/SecurityConfiguration.java
@@ -31,68 +31,69 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfiguration {
 
 
-    @Bean
-    @Order(1)
-    public SecurityFilterChain appSecurityFilterChain(HttpSecurity http) throws Exception {
-        //这里可以按照自己的需求配置不同的SecurityConfigurer,包括认证方式等等信息
-        http.securityMatcher("/app/**")
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults())
-                .formLogin(withDefaults())
-                .exceptionHandling((exceptionHandling) ->
-                        exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
-                                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                                    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-                                    response.setContentType(MediaType.APPLICATION_JSON.toString());
-                                    response.getWriter().write("{\"message:\":\"您无权访问01\"}");
-                                })
-                                .accessDeniedHandler((request, response, accessDeniedException) -> {
-                                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                                    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-                                    response.setContentType(MediaType.APPLICATION_JSON.toString());
-                                    response.getWriter().write("{\"message:\":\"您无权访问02\"}");
-                                }))
-        ;
-        return http.build();
-    }
-
-    @Bean
-    @Order(2)
-    public SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
-        //这里可以按照自己的需求配置不同的SecurityConfigurer,包括认证方式等等信息
-        http.securityMatcher("/web/**")
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults())
-                .formLogin(withDefaults());
-        return http.build();
-    }
-
-
 //    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//        http
+//    @Order(1)
+//    public SecurityFilterChain appSecurityFilterChain(HttpSecurity http) throws Exception {
+//        //这里可以按照自己的需求配置不同的SecurityConfigurer,包括认证方式等等信息
+//        http.securityMatcher("/app/**")
 //                .authorizeHttpRequests((authorize) -> authorize
 //                        .anyRequest().authenticated()
 //                )
+//                .httpBasic(withDefaults())
+//                .formLogin(withDefaults())
+//                .exceptionHandling((exceptionHandling) ->
+//                        exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
+//                                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+//                                    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+//                                    response.setContentType(MediaType.APPLICATION_JSON.toString());
+//                                    response.getWriter().write("{\"message:\":\"您无权访问01\"}");
+//                                })
+//                                .accessDeniedHandler((request, response, accessDeniedException) -> {
+//                                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+//                                    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+//                                    response.setContentType(MediaType.APPLICATION_JSON.toString());
+//                                    response.getWriter().write("{\"message:\":\"您无权访问02\"}");
+//                                }))
+//        ;
+//        return http.build();
+//    }
 //
-////                .authorizeHttpRequests(new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
-////                    @Override
-////                    public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
-////                        authorizationManagerRequestMatcherRegistry
-//////                                .requestMatchers("/favicon.ico").permitAll()
-////                                .requestMatchers("/code").permitAll()
-////                                .anyRequest().authenticated();
-////                    }
-////                })
+//    @Bean
+//    @Order(2)
+//    public SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
+//        //这里可以按照自己的需求配置不同的SecurityConfigurer,包括认证方式等等信息
+//        http.securityMatcher("/web/**")
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .anyRequest().authenticated()
+//                )
 //                .httpBasic(withDefaults())
 //                .formLogin(withDefaults());
 //        return http.build();
 //    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .anyRequest().authenticated()
+//                )
+
+                .authorizeHttpRequests(new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+                    @Override
+                    public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
+                        authorizationManagerRequestMatcherRegistry
+//                                .requestMatchers("/favicon.ico").permitAll()
+                                .requestMatchers("/code").permitAll()
+//                                .requestMatchers("/code1").access();
+                                .anyRequest().authenticated();
+                    }
+                })
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults());
+        return http.build();
+    }
 
 
     @Bean
